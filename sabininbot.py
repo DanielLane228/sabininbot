003e379f3ad3be35a44f61cde050cdde772092f6
DanielLane228 <57570564+DanielLane228@users.noreply.github.com>
2019-11-09 23:09:48 +0300

    Update sabininbot.py

--- sabininbot.py
+++ sabininbot.py
@@ -15,17 +15,13 @@ now = utcnow.astimezone(pytz.timezone('Asia/Chongqing'))
 def send_welcome(message):
     bot.reply_to(message, 'Ты опять время забыла да?' + emoji.emojize('\U0001F644'))
     bot.send_message(message.chat.id, str(now.strftime("%H:%M:%S, %A, %B %d, %Y")))
-    print(message.chat.id)
-    starttime=time.time()
-    while True:
-        if russia.day == 10 and russia.month == 11:
-            bot.send_message(message.chat.id, 'Happy birthday you still cutie little pie :3')
-            x = random.randint(1, 8)
-            photo = open(f'{x}.jpg', 'rb')
-            bot.send_photo(sabina, photo)
-            audio = open('Ирина_Аллегрова_С_днем_рождения!.mp3', 'rb')
-            bot.send_audio(sabina, audio)
-        time.sleep(86400 - ((time.time() - starttime) % 86400))
+    if russia.day == 10 and russia.month == 11:
+        bot.send_message(message.chat.id, 'Happy birthday you still cutie little pie :3')
+        x = random.randint(1, 8)
+        photo = open(f'{x}.jpg', 'rb')
+        bot.send_photo(sabina, photo)
+        audio = open('Ирина_Аллегрова_С_днем_рождения!.mp3', 'rb')
+        bot.send_audio(sabina, audio)
 
 
 bot.polling(none_stop=True, timeout=60)
